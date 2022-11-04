1a0bbe71ef4980b87d99ed6428835a0c3642cab8
= <=>
2022-11-04 12:43:03 -0400

    Files added

---
+++ delete_user.php
@@ -0,0 +1,16 @@
+<?php
+require_once('conn.php');
+$conn = DBConnect();
+$id = $_GET['id'];
+
+$sql = "DELETE from tbl_users WHERE id='$id' ";
+
+$results = mysqli_query($conn,$sql);
+if($results == true){
+    header("Location: index.php?msg=Record Deleted Successfully");
+}else{
+    echo  "Error: " . mysqli_error();
+}
+
+
+?>
